bf88df66e6a1894680172dd7e45265602cfa0969
prakash <prakashvijay4964@gmail.com>
2021-11-11 23:00:34 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from "./components/Container/Container"
+
+
+const App = () => {
+  return (
+    <div>
+      <Container />
+    </div>
+  )
+}
+
+export default App
+
